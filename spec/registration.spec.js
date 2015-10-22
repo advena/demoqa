8e34c0a4b4f6a24a932f1ba3950f0132921cb31c
advena <m.gepert@gmail.com>
2015-10-22 12:11:17 +0200

    szkielet testu dla registation

---
+++ spec/registration.spec.js
@@ -0,0 +1,45 @@
+'use strict';
+
+describe('user registration page', function() {
+	it('should have proper breadcrumb', function() {
+		
+	});
+	
+	it('should show error no empty form fields', function() {
+		
+	});
+	
+	it('should show error on invalid email', function() {
+		
+	});
+	
+	it('should show error on invalid phone number', function(){
+		
+	});
+	describe('when password is passed in', function() {
+		it('should show error on to short password', function() {
+			
+		});
+		
+		it('should show error on password missmatch', function() {
+			
+		});
+		
+		it('should show alert on week passwords ', function() {
+			
+		});
+		
+	});
+	
+	describe('when all form values are correct', function() {
+		it('should show confirmation tab', function() {
+			
+		});
+		
+		it('should show error tab on when user already exists', function() {
+			
+		});
+		
+	})
+	
+})
